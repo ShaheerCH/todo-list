ac068d305eb2b3075a23f0b13cdaa3f85433bb1e
shaheer ch <shaheer.chry@gmail.com>
2022-06-11 04:30:04 +0500

    Fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.css';
+import '/style.css';
 import newTaskListener from './modules/add-task.js';
 import renderUI from './modules/render.js';
 import delCompleted from './modules/clearAll.js';
